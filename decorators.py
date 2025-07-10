0e94be6acec8d041523fde7ad44434500a34daef
Bakhrombek Kadamov <bkadamov2509@gmail.com>
2025-07-10 21:54:24 +0000

    first commit at new branch from linux

--- decorators.py
+++ decorators.py
@@ -1,14 +1,25 @@
 def announce(f):
     def wrapper():
-        print("About to run function ...")
+        print("Function")
         f()
-        print("Done with the function")
+        print("Done function")
     return wrapper
-
+def announce1(f):
+    def wrapper1():
+        print("It'll be string")
+        f()
+        print("Done with string")
+    return wrapper1
 
 @announce
 def hello():
-    print("Hello,world!")
+    print("Assalomu Aleykum, Xush Kelibsizlar")
 
 
 hello()
+
+@announce1
+def salom():
+    print("Nima hizmat kerak")
+
+salom()
